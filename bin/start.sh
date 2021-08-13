6ddf3fe137f568d48a583a42c57faf4f4cef6191
Edward Phillips <44973397+ed1001@users.noreply.github.com>
2021-08-13 14:51:39 +0200

    suppress bash output

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-brew list sdl2 || brew install sdl2
-if ! bundle check; then
-  bundle install
+brew list sdl2 >/dev/null 2>/dev/null || (echo "installing sdl2..." && brew install sdl2 >/dev/null)
+if ! bundle check >/dev/null 2>/dev/null; then
+  echo "installing dependencies..."
+  bundle install >/dev/null
 fi
 ruby main.rb
